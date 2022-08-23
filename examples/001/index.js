fa110fa2d3adf0c3476ea04e34b7fa45e568f43b
Vinlic <vinlic@vinlic.com>
2022-08-24 00:05:55 +0800

    示例优化

--- examples/001/index.js
+++ examples/001/index.js
@@ -4,7 +4,7 @@ const path = require("path");
 const { Document, elements: { Voice, Prosody, Paragraph, Break } } = require("../../dist");
 
 const document = new Document();
-const voice = new Voice({ name: "test" });
+const voice = new Voice({ name: "zh-CN-XiaoxiaoNeural" });
 document.appendChild(voice);
 const prosody = new Prosody({ rate: 1.2 });
 voice.appendChild(prosody);
@@ -14,8 +14,45 @@ p.appendChild(new Break({ time: 2000 }));
 p.appendChild("再测试一下");
 prosody.appendChild(p);
 
-const result = document.render({
+fs.writeFileSync(path.join(__dirname, "aggregation.xml"), document.render({
     pretty: true
-});
+}));
 
-fs.writeFileSync(path.join(__dirname, "result.xml"), result);
+fs.writeFileSync(path.join(__dirname, "w3c.xml"), document.render({
+    pretty: true,
+    provider: "w3c"
+}));
+
+fs.writeFileSync(path.join(__dirname, "aliyun.xml"), document.render({
+    headless: true,
+    pretty: true,
+    provider: "aliyun"
+}));
+
+fs.writeFileSync(path.join(__dirname, "microsoft.xml"), document.render({
+    pretty: true,
+    provider: "microsoft"
+}));
+
+fs.writeFileSync(path.join(__dirname, "tencent.xml"), document.render({
+    headless: true,
+    pretty: true,
+    provider: "tencent"
+}));
+
+fs.writeFileSync(path.join(__dirname, "amazon.xml"), document.render({
+    headless: true,
+    pretty: true,
+    provider: "amazon"
+}));
+
+fs.writeFileSync(path.join(__dirname, "google.xml"), document.render({
+    headless: true,
+    pretty: true,
+    provider: "google"
+}));
+
+fs.writeFileSync(path.join(__dirname, "yunXiaoWei.xml"), document.render({
+    headless: true,
+    provider: "yunXiaoWei"
+}));
--- examples/002/index.js
+++ examples/002/index.js
@@ -1,22 +1,14 @@
 const fs = require("fs");
 const path = require("path");
 
-const { Document, elements: { Voice, Prosody, Paragraph, Break } } = require("../../dist");
+const { Document } = require("../../dist");
 
-const document = new Document();
-const voice = new Voice({ name: "aixia" });
-document.appendChild(voice);
-const prosody = new Prosody({ rate: 1.2 });
-voice.appendChild(prosody);
-const p = new Paragraph();
-p.appendChild("测试一下");
-p.appendChild(new Break({ time: 2000 }));
-p.appendChild("再测试一下");
-prosody.appendChild(p);
+const content = fs.readFileSync(path.join(__dirname, "ssml.xml")).toString();
+
+const document = Document.parse(content);
 
 const result = document.render({
-    pretty: true,
-    provider: "aliyun"
+    pretty: true
 });
 
 fs.writeFileSync(path.join(__dirname, "result.xml"), result);
