f09088eeaadc6299697b973721f5a12fc03638f4
Vinlic <vinlic@vinlic.com>
2022-09-08 14:34:52 +0800

    update

--- src/elements/Prosody.ts
+++ src/elements/Prosody.ts
@@ -59,8 +59,8 @@ export default class Prosody extends Element {
                     volume: util.isFinite(Number(options.volume)) ? Number(options.volume) * 0.5 : options.volume
                 };
             case ServiceProvider.YunXiaoWei:
-                options.rate = options.rate * 0.5 - 0.5;
-                options.pitch = options.pitch * 0.5 - 0.5;
+                options.rate = options.rate * 0.5 + 0.5;
+                options.pitch = options.pitch * 0.5 + 0.5;
         }
         return options;
     }
