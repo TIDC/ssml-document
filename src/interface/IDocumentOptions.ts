c0edefc06cf8d9953c02084f18f2cc9738f3ceb1
chenwb <chenwb@thinkive.com>
2023-01-03 13:45:19 +0800

    修复指定预期格式

--- src/interface/IDocumentOptions.ts
+++ src/interface/IDocumentOptions.ts
@@ -10,7 +10,7 @@ export default interface IDocumentOptions extends IBaseOptions {
     "xml:base"?: string;  //文档基础URL
     baseUrl?: string;  //文档基础URL扩展属性
     xmlns?: string;  //文档URI
-    encodeType?: string;  //音频编码类型
+    format?: string;  //音频编码类型
     sampleRate?: string;  //音频采样率
     bitrate?: string;  //音频码率
     provider?: ServiceProvider;  //预期产出提供商
